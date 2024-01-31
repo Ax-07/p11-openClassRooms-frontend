097c0b5943936e25d70103ec70162007b5c85199
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-31 14:25:21 +0100

    ajout routes et pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import './App.css';
-import { Navbar } from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Footer } from './layouts/Footer';
-function App() {
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Footer } from "./layouts/Footer";
+import { User } from "./pages/User";
 
+function App() {
   return (
     <>
-    <Navbar />
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </main>
-    <Footer />
+      <Navbar />
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
