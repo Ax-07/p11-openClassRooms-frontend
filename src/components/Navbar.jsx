44ce3f5233d79c75482540e162f786e335a0f4a2
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-01-31 14:25:05 +0100

    ajout navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,36 @@
-import logo from '../../public/img/argentBankLogo.png';
+import logo from "../../public/img/argentBankLogo.png";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+  return (
+    <nav className="main-nav">
+      <NavLink className="main-nav-logo" to={"/"}>
         <img
           className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </NavLink>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </a>
+        <NavLink
+          className="main-nav-item"
+          activeClassName="active"
+          to={"/login"}
+        >
+          <i className="fa fa-user-circle"></i> Sign In
+        </NavLink>
       </div>
+      {/* <div>
+        <a class="main-nav-item" href="./user.html">
+          <i class="fa fa-user-circle"></i>
+          Tony
+        </a>
+        <a class="main-nav-item" href="./index.html">
+          <i class="fa fa-sign-out"></i>
+          Sign Out
+        </a>
+      </div> */}
     </nav>
-    );
+  );
 };
