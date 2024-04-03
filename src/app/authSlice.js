e04b114448b7865587c4998bdc5ef2151626b03e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-04-03 16:26:25 +0200

    Mise à jour de l'authentification pour stocker le token dans le localStorage

--- src/app/authSlice.js
+++ src/app/authSlice.js
@@ -3,20 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    token: null,
-    isConnected: false
+    token: localStorage.getItem("token"),
+    isConnected: !!localStorage.getItem("token"),
   },
   reducers: {
     setIsConnected: (state, action) => {
       state.token = action.payload;
       state.isConnected = true;
+      localStorage.setItem("token", action.payload);
     },
     logout: (state) => {
         state.token = null;
         state.isConnected = false;
+        localStorage.removeItem("token");
     }
   },
 });
 
-export const { setIsConnected, logout } = authSlice.actions; // equivalent a une methode de class
+export const { setIsConnected, logout } = authSlice.actions;
 export default authSlice.reducer;
